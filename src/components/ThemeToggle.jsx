fd4589ca21bab184f6f07cbc3c551a180f8bdbee
Sheehan <sheehansuneel5@gmail.com>
2025-06-02 20:20:40 +0100

    fixed dark mode on mobile version

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -31,7 +31,7 @@ export const ThemeToggle = () => {
         }
     }
     
-    return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+    return <button onClick={toggleTheme} className={cn("fixed top-15 right-8 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-hidden"
     )}>
         {isDarkMode ? (
